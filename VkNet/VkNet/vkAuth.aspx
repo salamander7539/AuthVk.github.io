0f7367e4daca95ba0199e3a5d804433eb3fbf0c0
salamander7539 <48787344+salamander7539@users.noreply.github.com>
2020-01-28 15:04:30 +0300

    vk commit

---
+++ VkNet/VkNet/vkAuth.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="vkAuth.aspx.cs" Inherits="VkNet.vkAuth" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" style="z-index: 1; left: 10px; top: 15px; position: absolute" Text="Login"></asp:Label>
+            <asp:Label ID="Label3" runat="server" style="z-index: 1; left: 262px; top: 15px; position: absolute" Text="Friends List"></asp:Label>
+        </div>
+        <asp:TextBox ID="log_textBox" runat="server" style="z-index: 1; left: 10px; top: 44px; position: absolute; margin-top: 0px" TextMode="Password"></asp:TextBox>
+        <asp:TextBox ID="pass_textBox" runat="server" style="z-index: 1; left: 9px; top: 98px; position: absolute" TextMode="Password"></asp:TextBox>
+        <asp:ListBox ID="ListBox1" runat="server" style="z-index: 1; left: 259px; top: 34px; position: absolute; height: 271px; width: 187px; margin-top: 4px"></asp:ListBox>
+        <asp:Button ID="auth_Button" runat="server" OnClick="auth_Button_Click" style="z-index: 1; left: 8px; top: 133px; position: absolute" Text="Authorization" />
+        <asp:Label ID="Label2" runat="server" style="z-index: 1; left: 9px; top: 74px; position: absolute" Text="Password"></asp:Label>
+        <asp:Label ID="Status_Label" runat="server" style="z-index: 1; left: 13px; top: 175px; position: absolute" Text="Login status:"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ VkNet/VkNet/vkAuth.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+using xNet;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+
+namespace VkNet
+{
+    public partial class vkAuth : System.Web.UI.Page
+    {
+        string token, id;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void auth_Button_Click(object sender, EventArgs e)
+        {  
+            Authoriz();
+        }
+
+        void Authoriz()
+        {
+            try
+            {
+                var ent_data = new HttpRequest();
+                string response = ent_data.Get("https://oauth.vk.com/token?grant_type=password&client_id=2274003&client_secret=hHbZxrka2uZ6jB1inYsH&username=" + log_textBox.Text + "&password=" + pass_textBox.Text).ToString();
+                dynamic json = JObject.Parse(response);
+                token = json.access_token;
+                id = json.user_id;
+                if (id.Count() > 1)
+                {
+                    Status_Label.Text = Status_Label.Text + " OK!";
+                    GetName();
+                    GetFriends();
+                }
+            }
+            catch
+            {
+                Status_Label.Text = Status_Label.Text + " Wrong login or password!";
+            }
+        }
+
+        void OpenMyData()
+        {
+            if (File.Exists(@"D:\C#Apps\LoadVKData.txt"))
+            {
+                string[] aut_data = File.ReadAllLines(@"D:\C#Apps\LoadVKData.txt");
+                log_textBox.Text = aut_data[0];
+                pass_textBox.Text = aut_data[1];
+            }
+        }
+
+        void SaveMyData()
+        {
+            if (!File.Exists(@"D:\C#Apps\LoadVKData.txt"))
+            {
+                string[] VkData = new string[]
+                {
+                    log_textBox.Text,
+                    pass_textBox.Text
+                };
+                File.Create(@"D:\C#Apps\LoadVKData.txt").Close();
+                File.WriteAllLines(@"D:\C#Apps\LoadVKData.txt", VkData);
+            }
+            else
+            {
+                File.Delete(@"D:\C#Apps\LoadVKData.txt");
+                string[] VkData = new string[]
+                {
+                    log_textBox.Text,
+                    pass_textBox.Text
+                };
+                File.Create(@"D:\C#Apps\LoadVKData.txt").Close();
+                File.WriteAllLines(@"D:\C#Apps\LoadVKData.txt", VkData);
+            }
+        }
+
+        public void GetName()
+        {
+            var qwer = new HttpRequest();
+            string response = qwer.Get("https://api.vk.com/method/account.getProfileInfo?v=5.52&access_token=" + token).ToString();
+            dynamic json = JObject.Parse(response);
+            string f_name = json.response.first_name;
+            string l_name = json.response.last_name;
+            Status_Label.Text = Environment.NewLine + f_name + " " + l_name;
+        }
+
+        public void GetFriends()
+        {
+            ListBox1.Items.Clear();
+            var qwer = new HttpRequest();
+            string response = qwer.Get("https://api.vk.com/method/friends.search?v=5.52&count=5&access_token=" + token).ToString();
+            dynamic json = JObject.Parse(response);
+
+            int count = json.response.count;
+            string[] fname = new string[count];
+            string[] lname = new string[count];
+            for (int i = 0; i < 5; i++)
+            {
+                fname[i] = json["response"]["items"][i]["first_name"];
+                lname[i] = json["response"]["items"][i]["last_name"];
+                ListBox1.Items.Add(fname[i] + " " + lname[i] + Environment.NewLine);
+            }
+        }
+    }
+}
